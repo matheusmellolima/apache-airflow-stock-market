478fe3e66de725bf0c8a025289efecb9209a6f9a
Matheus Lima <matheus.lima@wexinc.com>
2024-10-04 16:33:57 -0300

    Add loads data to warehouse

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -7,8 +7,16 @@ from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
 
-from include.stock_market.tasks import _get_stock_prices, _store_stock_prices
+from include.stock_market.tasks import (
+    _get_stock_prices,
+    _store_stock_prices,
+    _get_transformed_stock_market_data,
+    S3_BUCKET_NAME,
+)
 
 
 SYMBOL = "NVDA"  # Nvidia stock symbol
@@ -79,8 +87,39 @@ def stock_market():  # This will be the DAG ID in Airflow
         }
     )
 
+    get_transformed_stock_market_data = PythonOperator(
+        task_id="get_transformed_stock_market_data",
+        python_callable=_get_transformed_stock_market_data,
+        op_kwargs={
+            'csv_file_location': "{{ task_instance.xcom_pull(task_ids='store_stock_prices') }}"
+        }
+    )
+
+    # TODO:
+    # FIX raise ValueError(f"Unsupported filetype '{extension}' from file '{filepath}'.")
+    # ValueError: Unsupported filetype
+    # 'xcom_pull(task_ids='get_transformed_stock_market_data') }'
+    # from file
+    # 's3://stock-market/{ task_instance.xcom_pull(task_ids='get_transformed_stock_market_data') }'.
+
+    load_data_to_warehouse = aql.load_file(
+        task_id="load_data_to_warehouse",
+        input_file=File(
+            path=f"{
+                S3_BUCKET_NAME}/{{ task_instance.xcom_pull(task_ids='get_transformed_stock_market_data') }}",
+            conn_id="minio"
+        ),
+        output_table=Table(
+            name="stock_market",
+            conn_id="postgres",
+            metadata=Metadata(
+                schema="public",
+            )
+        )
+    )
+
     # Define the task dependencies and execution order
-    is_api_available() >> get_stock_prices >> store_stock_prices >> stock_prices_transform
+    is_api_available() >> get_stock_prices >> store_stock_prices >> stock_prices_transform >> get_transformed_stock_market_data >> load_data_to_warehouse
 
 
 stock_market()  # This will register the DAG in Airflow
--- include/stock_market/tasks.py
+++ include/stock_market/tasks.py
@@ -3,10 +3,26 @@ from io import BytesIO
 import json
 import requests
 from airflow.hooks.base import BaseHook
+from airflow.exceptions import AirflowNotFoundException
 from minio import Minio
 
 
-def _get_stock_prices(symbol: str):
+BUCKET_NAME = "stock-market"
+S3_BUCKET_NAME = f"s3://{BUCKET_NAME}"
+
+
+def _get_minio_client() -> Minio:
+    minio = BaseHook.get_connection('minio')
+    client = Minio(
+        endpoint=minio.extra_dejson["endpoint"].split("//")[1],
+        access_key=minio.login,
+        secret_key=minio.password,
+        secure=False
+    )
+    return client
+
+
+def _get_stock_prices(symbol: str) -> str:
     """Get stock prices from the API."""
     api = BaseHook.get_connection('stock_market_api')
     base_url = f"{api.host}{api.extra_dejson['endpoint']}"
@@ -17,27 +33,32 @@ def _get_stock_prices(symbol: str):
     return json.dumps(result)
 
 
-def _store_stock_prices(stock: str):
+def _store_stock_prices(stock: str) -> str:
     """Store stock prices in the Minio bucket."""
-    minio = BaseHook.get_connection('minio')
-    client = Minio(
-        endpoint=minio.extra_dejson["endpoint"].split("//")[1],
-        access_key=minio.login,
-        secret_key=minio.password,
-        secure=False
-    )
-    bucket_name = "stock-market"
-    if not client.bucket_exists(bucket_name):
-        client.make_bucket(bucket_name)
+    minio = _get_minio_client()
+    if not minio.bucket_exists(BUCKET_NAME):
+        minio.make_bucket(BUCKET_NAME)
 
     stock = json.loads(stock)
     symbol = stock["meta"]["symbol"]
     data = json.dumps(stock, ensure_ascii=False).encode('utf-8')
     # Store the data in the database
-    obj = client.put_object(
-        bucket_name=bucket_name,
+    obj = minio.put_object(
+        bucket_name=BUCKET_NAME,
         object_name=f"{symbol}/prices.json",
         data=BytesIO(data),
         length=len(data)
     )
     return f"{obj.bucket_name}/{symbol}"
+
+
+def _get_transformed_stock_market_data(csv_file_location: str = "stock-market/NVDA"):
+    minio = _get_minio_client()
+    prefix = f"{csv_file_location.split("/")[1]}/transform/"
+    objects = minio.list_objects(BUCKET_NAME, prefix=prefix, recursive=True)
+    for obj in objects:
+        if obj.object_name.endswith(".csv"):
+            return obj.object_name
+
+    raise AirflowNotFoundException(
+        f"No transformed data found for {csv_file_location}")
